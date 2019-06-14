5e041e766341ca3bee9d395516a23ef38a452666
Colin <cstatro@gmail.com>
2019-06-13 20:21:29 -0400

    all tests passing

--- mylib.js
+++ mylib.js
@@ -2,7 +2,11 @@ function helloWorld() {
   return "Hello there";
 }
 
-function formatName(str) {}
+function formatName(str) {
+  let splitName = str.split(",");
+  splitName[1] = splitName[1].trim();
+  return `${splitName[1]} ${splitName[0]}`;
+}
 
 module.exports = {
   helloWorld: helloWorld,
